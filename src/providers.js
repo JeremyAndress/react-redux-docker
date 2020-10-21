97d3f5b5874336e919d0e3a3ac3f117fb2fc2b65
JeremySilvaSilva <jeremysilvasilva@gmail.com>
2020-10-21 16:29:52 -0300

    ADD: remove provider

--- src/providers.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const CardInfo = React.createContext({
-    title: 'Pop Food',
-    artist: 'Jack Stauber',
-    genre: 'Edible Pop', // lol
-});
-export default CardInfo;
